f8104b6005bc8cc045a0c2a74a547bcc46aaa53b
Sahil <sahilaroraji2002@gmail.com>
2024-03-12 12:19:03 +0530

    adding cors

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ connectToMongoose(process.env.MONGO_URL)
   .catch((err) => console.error(err));
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Request received:', req.method, req.url);
+  next();
+});
+
+
 app.use(cors({
   origin: 'https://web-url-shortner.netlify.app',
   methods: ['GET', 'POST'], 
@@ -26,7 +33,9 @@ app.use("/:shortid", async (req, res) => {
     { $push: { visitHistory: { timeStamp: Date.now() } } }
   );
   console.log(entry);
-
+  res.setHeader('Access-Control-Allow-Origin', 'https://web-url-shortner.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.redirect(entry.redirectUrl);
 });
 
